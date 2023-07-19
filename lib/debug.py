3d475919295f74cccac8fed96bcda9db349b4bec
Alexis Boucouvalas <alexisbou16@gmail.com>
2023-07-19 11:08:19 -0400

    Keep track of users

--- lib/debug.py
+++ lib/debug.py
@@ -15,31 +15,36 @@ def insert_words(file_path):
 
 
 
-# def insert_username():
-    # username = input('Enter Username Here!')
-
-    # new_user = User(username=username)
-    # session.add(new_user)
-    # session.commit()
-    # session.close()
-
 def play():
-    username = input('Enter Username Here!')
+    # ---START OF GAME: GET USERNAME---
+    username = input('Enter Username Here: ')
+    user = session.query(User).filter_by(username=username).first()
 
-    new_user = User(username=username)
-    session.add(new_user)
-    session.commit()
-    session.close()
-    print(f'Hello, {username}, would you like to play a game?')
+    # If user already exists in the User model, say welcome back
+    if user:
+        print(f'Welcome back, {username}!')
+
+    # If user doesn't already exist, create a new user and add to the User model. Say hello to new user.
+    else:
+        new_user = User(username=username)
+        session.add(new_user)
+        session.commit()
+        session.close()
+        print(f'Hello, {username}, would you like to play a game?')
     
+    # ---GETTING A RANDOM WORD FROM THE GAME MODEL---
     query = session.query(Game)
     count = query.count()
 
+    # Handle the case where the table is empty
     if count == 0:
-         return None  # Handle the case where the table is empty
+         return None  
 
+    # pick a random row between the first and last indices, inclusive
     random_offset = random.randint(0, count - 1)
     random_row = query.offset(random_offset).first()
+
+    # generate a blank space for each letter in the word
     word_blanks = " _ " * len(random_row.word)
 
     print(f"Random Row ID: {random_row.id}, Name: {random_row.word}")
